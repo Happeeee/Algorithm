ef618aaa89e66842a79231f151163af5c8bc2811
Happeeee <106572156+Happeeee@users.noreply.github.com>
2023-06-22 20:07:42 +0900

    Q1 모험가길드

---
+++ 이것이 코딩테스트다/Greedy/Q1_모험가길드.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+
+int main() {
+	int n, x, count = 0, group = 0;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		v.push_back(x);
+	}
+
+	sort(v.begin(), v.end()); // 오름차순 정렬해서 앞에서 부터 검사
+
+	for (int i = 0; i < v.size(); i++) {
+		count++;
+
+		if (v[i] <= count) { // 핵심 로직 : 현재 공포도가 현재 그룹원 수보다 작다면 그룹 결성, 아니라면 더 가보기
+			group++;
+			count = 0;
+		}
+	}
+
+	cout << group;
+}
