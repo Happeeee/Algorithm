1e06ed9797288f870ec90789161d9cfea053a640
Happeeee <106572156+Happeeee@users.noreply.github.com>
2023-07-14 16:33:13 +0900

    Create 18_모의고사.java

---
+++ 프로그래머스 문제풀이 전략/18_모의고사.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    private static final int[] candidate1 = {1, 2, 3, 4, 5};
+    private static final int[] candidate2 = {2, 1, 2, 3, 2, 4, 2, 5};
+    private static final int[] candidate3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
+    public int[] solution(int[] answers) {
+        int[] counts = new int[3];
+        List<Integer> list = new ArrayList<>();
+        int max = -1;
+    
+        for(int i = 0; i < answers.length; i++) {
+            if(answers[i] == candidate1[i % 5])
+                counts[0]++;
+            if(answers[i] == candidate2[i % 8])
+                counts[1]++;
+            if(answers[i] == candidate3[i % 10])
+                counts[2]++;            
+        }
+        
+        for(int i = 0; i < 3; i++) {
+            if(counts[i] > max)
+                max = counts[i];                
+        }
+        
+        for(int i = 0; i < 3; i++) {
+            if(counts[i] == max)
+                list.add(i + 1);
+        }
+                
+        int[] result = new int[list.size()];
+        
+        int index = 0;
+        for(int i : list)
+            result[index++] = i;
+                        
+        return result;
+    }
+}
