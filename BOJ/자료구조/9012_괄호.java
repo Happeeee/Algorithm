f9c7a0536144f3fe96d28872ff0456fc8c30b9c7
Happeeee <106572156+Happeeee@users.noreply.github.com>
2023-07-27 01:33:12 +0900

    Create 9012_괄호.java

---
+++ BOJ/자료구조/9012_괄호.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+
+            Stack<Character> s = new Stack<>();
+            boolean isVps = true;
+
+            for(char c : str.toCharArray()) {
+                if(c == ')') {
+                    if(s.isEmpty()) isVps = false;
+                    else s.pop();
+                } else
+                    s.push(c);
+            }
+
+            if(s.isEmpty() && isVps) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
+
