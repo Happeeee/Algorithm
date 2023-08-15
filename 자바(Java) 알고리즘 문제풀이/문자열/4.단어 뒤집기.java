13c9db26554aaa8ca0839a50ea0adff546234414
Happeeee <106572156+Happeeee@users.noreply.github.com>
2023-08-15 17:06:53 +0900

    Update 4.단어 뒤집기.java

--- 자바(Java) 알고리즘 문제풀이/문자열/4.단어 뒤집기.java
+++ 자바(Java) 알고리즘 문제풀이/문자열/4.단어 뒤집기.java
@@ -14,3 +14,28 @@ public class Main {
         }
     }
 }
+
+// reverse 직접 구현
+// public class Main {
+//     public static String solution(String s) {
+//         char[] arr = s.toCharArray();
+//         int lt = 0, rt = arr.length - 1;
+//         while(lt < rt) {
+//             char temp = arr[lt];
+//             arr[lt] = arr[rt];
+//             arr[rt] = temp;
+//             lt++;
+//             rt--;
+//         }
+//         return String.valueOf(arr);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = 0; i < n; i++) {
+//             String s = sc.next();
+//             System.out.println(solution(s));
+//         }
+//     }
+// }
