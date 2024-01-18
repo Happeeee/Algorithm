464850f28b5e17288e60cea5f0c503b866581025
Happeeee <106572156+Happeeee@users.noreply.github.com>
2024-01-18 23:10:13 +0900

    Create 11022_A+B - 8.java

---
+++ BOJ/입출력/11022_A+B - 8.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= n; i++) {
+            String str = br.readLine();
+            int a = str.charAt(0) - '0';
+            int b = str.charAt(2) - '0';
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
+        }
+    }
+}
