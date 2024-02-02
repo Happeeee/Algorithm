c952f78db9d53d76546f71cec9bb8f2fc7a27978
Happeeee <106572156+Happeeee@users.noreply.github.com>
2024-02-02 23:09:44 +0900

    Create 2. 버블 정렬.java

---
+++ 자바(Java) 알고리즘 문제풀이/정렬/2. 버블 정렬.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i : solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static int[] solution(int n, int[] arr) {
+        for(int i = n - 1; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
