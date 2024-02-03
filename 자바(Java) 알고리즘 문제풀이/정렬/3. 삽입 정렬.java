f26da40673d07695f59e1c101e2b6335edc36417
Happeeee <106572156+Happeeee@users.noreply.github.com>
2024-02-03 15:31:52 +0900

    Create 3. 삽입 정렬.java

---
+++ 자바(Java) 알고리즘 문제풀이/정렬/3. 삽입 정렬.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i : solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static int[] solution(int n, int[] arr) {
+        for(int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for(j = i - 1; j >= 0 && arr[j] > temp; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = temp;
+        }
+        return arr;
+    }
+}
