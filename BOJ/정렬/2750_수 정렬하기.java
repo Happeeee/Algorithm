e6bfa285588d822c70e9fe0dfbd24c928fe99424
Happeeee <106572156+Happeeee@users.noreply.github.com>
2024-02-11 17:00:37 +0900

    Update 2750_수 정렬하기.java

--- BOJ/정렬/2750_수 정렬하기.java
+++ BOJ/정렬/2750_수 정렬하기.java
@@ -3,23 +3,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {      
-        public static void main(String[] args) throws IOException {
-            BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-            StringBuilder sb = new StringBuilder();
-            
-            int n = Integer.parseInt(br.readLine());
-            int[] arr = new int[n];
-            
-            for(int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(br.readLine());
-            }
-            
-            Arrays.sort(arr);
-            
-            for(int i : arr) {
-                sb.append(i).append("\n");
-            }
-            System.out.println(sb);    
-        }         
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i : arr) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
