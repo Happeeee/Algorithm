5ff549c222f15a61861cce6d908975ff9219cccd
Happeeee <106572156+Happeeee@users.noreply.github.com>
2024-02-14 15:11:45 +0900

    Update 2178_미로탐색.java

--- BOJ/그래프이론/2178_미로탐색.java
+++ BOJ/그래프이론/2178_미로탐색.java
@@ -1,61 +1,69 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static int[][] map = new int[100][100];
-	private static int[][] visited = new int[100][100];
-	private static int[] dx = {0, 0, -1, 1};
-	private static int[] dy = {-1, 1, 0, 0};	
-	private static int n, m;
-	
-	private static class Pos {
-		int y; 
-		int x;
-		
-		private Pos(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
-	
-	private static void bfs(int y, int x) {
-		Queue<Pos> q = new LinkedList<>();
-		visited[y][x] = 1;
-		q.add(new Pos(y, x));
-		
-		while(!q.isEmpty()) {
-			Pos here = q.poll();			
-			
-			for(int i = 0; i < 4; i++) {
-				int ny = here.y + dy[i];
-				int nx = here.x + dx[i];
-				
-				if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-				if(map[ny][nx] == 0 || visited[ny][nx] != 0) continue;
-				
-				visited[ny][nx] = visited[here.y][here.x] + 1;
-				q.add(new Pos(ny, nx));				
-			}			
-		}		
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		m = sc.nextInt();
-		sc.nextLine();
-		
-		for(int i = 0; i < n; i++ ) {
-			String str = sc.nextLine();
-			for(int j = 0; j < m; j++) {
-				map[i][j] = str.charAt(j) - '0';
-			}
-		}
-		
-		bfs(0, 0);
-		
-		System.out.println(visited[n - 1][m - 1]);
-	}
+
+    static int[][] map;
+    static int[][] visited;
+    static final int[] dx = {0, 0, -1, 1};
+    static final int[] dy = {1, -1, 0, 0};
+
+    static int n;
+    static int m;
+
+    static class Point {
+        int y;
+        int x;
+
+        Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    static void bfs() {
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(1, 1));
+        visited[1][1] = 1;
+
+        while(!q.isEmpty()) {
+            Point p = q.poll();
+
+            for(int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+
+                if(nx < 1 || ny < 1 || nx > m || ny > n) continue;
+                if(map[ny][nx] == 0 || visited[ny][nx] != 0) continue;
+
+                visited[ny][nx] = visited[p.y][p.x] + 1;
+                q.add(new Point(ny, nx));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n + 1][m + 1];
+        visited = new int[n + 1][m + 1];
+
+        for(int i = 1; i <= n; i++) {
+            String s = br.readLine();
+            for(int j = 1; j <= m; j++) {
+                map[i][j] = s.charAt(j - 1) - '0';
+            }
+        }
+
+        bfs();
+
+        System.out.println(visited[n][m]);
+    }
 }
-
-
