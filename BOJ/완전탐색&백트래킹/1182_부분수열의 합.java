ecb17ad4fd31200d5d62e6f8fd5940736704c9cc
Happeeee <106572156+Happeeee@users.noreply.github.com>
2024-02-20 17:13:18 +0900

    Update 1182_부분수열의 합.java

--- BOJ/완전탐색&백트래킹/1182_부분수열의 합.java
+++ BOJ/완전탐색&백트래킹/1182_부분수열의 합.java
@@ -56,3 +56,48 @@ public class Main {
     }
 }
 
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.Arrays;
+// import java.util.StringTokenizer;
+
+// public class Main {
+
+//     static int n;
+//     static int s;
+//     static int count = 0;
+//     static int[] arr;
+
+//     public static void dfs(int depth, int sum) {
+//         if(depth == n) {
+//             if(sum == s) {
+//                 count++;
+//             }
+//             return;
+//         }
+//         dfs(depth + 1, sum);
+//         dfs(depth + 1, sum + arr[depth]);
+//     }
+
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine());
+
+//         n = Integer.parseInt(st.nextToken());
+//         s = Integer.parseInt(st.nextToken());
+
+//         arr = new int[n];
+
+//         st = new StringTokenizer(br.readLine());
+//         for(int i = 0; i < n; i++) {
+//             arr[i] = Integer.parseInt(st.nextToken());
+//         }
+
+//         dfs(0, 0);
+//         if(s == 0) {
+//             count -= 1;
+//         }
+//         System.out.println(count);
+//     }
+// }
