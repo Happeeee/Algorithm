072807daeceed21b5e2bf8b5f1e641852d405b1e
Happeeee <106572156+Happeeee@users.noreply.github.com>
2024-03-21 16:01:58 +0900

    Create 7785_회사에 있는 사람.java

---
+++ BOJ/자료구조/7785_회사에 있는 사람.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        TreeSet<String> set = new TreeSet<>(Collections.reverseOrder());
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String action = st.nextToken();
+
+            if(action.equals("enter")) {
+                set.add(name);
+            } else {
+                set.remove(name);
+            }
+        }
+
+        for(String s : set) {
+            System.out.println(s);
+        }
+    }
+}
